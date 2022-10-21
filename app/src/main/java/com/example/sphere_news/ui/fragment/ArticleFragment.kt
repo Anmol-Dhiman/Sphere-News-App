975df516e281c736ff94b8f90db4560d3233b8dd
Harsh Vardhan Singh Shekhawat <77687381+Harsh-Vardhan-Singh-Shekhawat@users.noreply.github.com>
2022-10-21 21:56:20 +0530

    fragment kotlin files

---
+++ app/src/main/java/com/example/sphere_news/ui/fragment/ArticleFragment.kt
@@ -0,0 +1,9 @@
+package com.example.news.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.news.R
+
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+    
+}
---
+++ app/src/main/java/com/example/sphere_news/ui/fragment/SearchNewsFragment.kt
@@ -0,0 +1,9 @@
+package com.example.news.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.news.R
+
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+
+}
